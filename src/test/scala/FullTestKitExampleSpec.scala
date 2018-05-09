d8fbb20060c9708ec3ae7baeef657e4bbb98541f
ernovnik <ernovnik@gmail.com>
2018-05-09 14:27:50 +0300

    refactoring

--- src/test/scala/FullTestKitExampleSpec.scala
+++ src/test/scala/FullTestKitExampleSpec.scala
@@ -4,54 +4,62 @@ import ciklum.backend.test.{Credentials, UserToken}
 import spray.json.DefaultJsonProtocol.{jsonFormat1, jsonFormat2}
 import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.HttpEntity
 import akka.http.scaladsl.server.Directives._
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import spray.json.DefaultJsonProtocol._
 import akka.testkit.TestDuration
-import ciklum.backend.test.actor.AsyncTokenIssueActor
-
+import ciklum.backend.test.actor.Supervisor
+import ciklum.backend.test.service.SimpleAsyncTokenService
 import scala.concurrent.duration._
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
-class FullTestKitExampleSpec extends WordSpec with Matchers with ScalatestRouteTest {
-
-
+class FullTestKitExampleSpec extends WordSpec with Matchers with ScalatestRouteTest with SimpleAsyncTokenService {
   implicit val actor_system = ActorSystem()
   implicit val actor_materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
   implicit val credentialsFormat = jsonFormat2(Credentials)
   implicit val userTokenFormat = jsonFormat1(UserToken)
-  val actor = system.actorOf(Props[AsyncTokenIssueActor], "token-issue-actor")
-  implicit val timeout = Timeout(7 seconds)
-  implicit val test_timeout = RouteTestTimeout(5.seconds dilated)
+  val supervisor = system.actorOf(Props[Supervisor], "supervisor")
+  implicit val timeout = Timeout(10 seconds)
+  implicit val test_timeout = RouteTestTimeout(10.seconds dilated)
+
+  override def requestToken(credentials: Credentials): Future[UserToken] = (supervisor ? credentials).mapTo[Future[UserToken]] flatten
 
   val route =
     post {
       entity(as[Credentials]) { credentials: Credentials =>
-        val future: Future[Future[UserToken]] = (actor ? credentials).asInstanceOf[Future[Future[UserToken]]]
-        onComplete(future.flatten) {
-          case Success(value) => complete(value)
-          case Failure(ex) => complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>${ex.getMessage}</h1>"))
+        val future = requestToken(credentials)
+        onComplete(future) {
+          case Success(value) => {
+            complete(value)
+          }
+          case Failure(ex) => {
+            complete(ex.getMessage)
+          }
         }
       }
     }
 
-  println(1)
-
-  Post("issue-token", Credentials("Nikita", "p@ssword")) ~> route ~> check {
-    responseAs[UserToken] should matchPattern {case UserToken(token) if token.startsWith("Nikita_")=>}
-  }
-  Post("issue-token", Credentials("Nikolay", "p@ssword")) ~> route ~> check {
-    responseAs[UserToken] should matchPattern {case UserToken(token) if token.startsWith("Nikolay_")=>}
-  }
-  Post("issue-token", Credentials("Test", "p@ssword")) ~> route ~> check {
-    responseAs[UserToken] should matchPattern {case UserToken(token) if token.startsWith("Test_")=>}
+  "successfully getting user token" in {
+    Post("issue-token", Credentials("user", "USER")) ~> route ~> check {
+      responseAs[UserToken] should matchPattern { case UserToken(token) if token.startsWith("user_") => }
+    }
   }
 
-  println(2)
+  "wrong username/password" in {
+    Post("issue-token", Credentials("user", "p@ssword")) ~> route ~> check {
+      responseAs[HttpEntity] should matchPattern { case HttpEntity.Strict(_,data) if(data.decodeString("UTF-8")=="wrong username/password" ) => }
+    }
+  }
+
+  "userId starts with A" in {
+    Post("issue-token", Credentials("Auser", "AUSER")) ~> route ~> check {
+      responseAs[HttpEntity] should matchPattern { case HttpEntity.Strict(_,data) if(data.decodeString("UTF-8")=="userId starts with A" ) => }
+    }
+  }
 
 }
